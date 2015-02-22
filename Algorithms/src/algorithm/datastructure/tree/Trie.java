e1aa95ae267e1bc2fe21ba26867c7d6057de5d53
Ivan-Zaitsau <ivan.zaitsau@gmail.com>
2015-02-22 16:49:19 -0300

    Trie class

--- Algorithms/src/algorithm/datastructure/tree/Trie.java
+++ Algorithms/src/algorithm/datastructure/tree/Trie.java
@@ -1,5 +1,28 @@
 package algorithm.datastructure.tree;
 
-public class Trie {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Trie {
+	
+	private Node root = new Node();
+	
+	private static class Node {
+		Map<Character, Node> children = new HashMap<>();
+	}
+	
+	public int add(CharSequence s) {
+		int prefix = 0;
+		Node node = root;
+		for (int i = 0; i < s.length(); i++) {
+			Character ch = s.charAt(i);
+			Node nextNode = node.children.get(ch);
+			if (nextNode == null) {
+				nextNode = new Node();
+				node.children.put(ch, nextNode);
+			}
+			else prefix++;
+		}
+		return prefix;
+	}
 }
