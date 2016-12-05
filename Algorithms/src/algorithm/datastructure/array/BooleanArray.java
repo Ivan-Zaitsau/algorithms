82e01812c9bcb537947de9371e0faa99105d67de
Ivan-Zaitsau <ivan.zaitsau@gmail.com>
2016-12-05 19:29:33 +0300

    BooleanArray.bitCount() method added

--- Algorithms/src/algorithm/datastructure/array/BooleanArray.java
+++ Algorithms/src/algorithm/datastructure/array/BooleanArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class BooleanArray {
 	
 	private static final int ADDRESS_BITS = 6;
-	private static final int BITS = 1 << ADDRESS_BITS;
+	private static final int MASK = (1 << ADDRESS_BITS) - 1;
 	
 	public final int length;
 	
@@ -17,11 +17,11 @@ public class BooleanArray {
 	}
 
 	public void setTrue(int index) {
-		data[index >>> ADDRESS_BITS] |= 1L << (index & (BITS - 1));
+		data[index >>> ADDRESS_BITS] |= 1L << (index & MASK);
 	}
 	
 	public void setFalse(int index) {
-		data[index >>> ADDRESS_BITS] &= ~(1L << (index & (BITS - 1)));
+		data[index >>> ADDRESS_BITS] &= ~(1L << (index & MASK));
 	}
 	
 	public void set(int index, boolean value) {
@@ -32,7 +32,30 @@ public class BooleanArray {
 	}
 	
 	public boolean get(int index) {
-		return (data[index >>> ADDRESS_BITS] & (1L << (index & (BITS - 1)))) != 0;
+		return (data[index >>> ADDRESS_BITS] & (1L << (index & MASK))) != 0;
+	}
+	
+	public int bitCount(int fromIndex, int toIndex) {
+		if (fromIndex >= toIndex)
+			return 0;
+		int fromIndexHigh = fromIndex >>> ADDRESS_BITS;
+		int fromIndexLow = fromIndex & MASK;
+		int toIndexHigh = toIndex >>> ADDRESS_BITS;
+		int toIndexLow = toIndex & MASK;
+		if (fromIndexHigh == toIndexHigh)
+			return Long.bitCount(data[fromIndexHigh] & ~((1L << fromIndexLow) - 1) & ((1L << toIndexLow) - 1));
+		int result = 0;
+		if (fromIndexLow > 0) {
+			result += Long.bitCount(data[fromIndexHigh] & ~((1L << fromIndexLow) - 1));;
+			fromIndexHigh++;
+		}
+		if (toIndexLow > 0) {
+			result += Long.bitCount(data[toIndexHigh] & ((1L << toIndexLow) - 1));
+		}
+		for (int i = fromIndexHigh; i < toIndexHigh; i++) {
+			result += Long.bitCount(data[i]);
+		}
+		return result;
 	}
 	
 	public void clear() {
