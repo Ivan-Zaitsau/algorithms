bcb3fee001e7484c883a68ed968636e50090bd21
Ivan-Zaitsau <ivan.zaitsau@gmail.com>
2017-01-16 13:10:18 +0300

    Simpler and faster version of nCk for IntegersModuloField

--- Algorithms/src/algorithm/numeric/IntegersModuloField.java
+++ Algorithms/src/algorithm/numeric/IntegersModuloField.java
@@ -1,6 +1,5 @@
 package algorithm.numeric;
 
-import java.util.Arrays;
 import java.util.BitSet;
 
 public class IntegersModuloField {
@@ -48,62 +47,7 @@ public class IntegersModuloField {
 		return fact;
 	}
 	
-	public int nCk(final int n, final int k) {
-		if (n-k < k)
-			return nCk(n, n-k);
-
-		BitSet isComposite = generateCompositeNumbersSet(k);
-		int[] divCounts = new int[k+1];
-		for (int i = 2; i <= k; i++)
-			if (!isComposite.get(i)) {
-				divCounts[i] -= factorialDivCounts(k, i);
-			}
-
-		int result = 1;
-		for (int i = n-k+1; i <= n; i++) {
-			for (int divisor : getDivisors(i, k)) {
-				if (divisor > k)
-					result = mul(result, divisor);
-				else
-					divCounts[divisor]++;
-			}
-		}
-		for (int divisor = 2; divisor <= k; divisor++)
-			if (divCounts[divisor] > 0)
-				result = mul(result, pow(divisor, divCounts[divisor]));
-		return result;
-	}
-	
-	// - helper methods
-	private static int[] getDivisors(int v, int limit) {
-		if (v <= 1)
-			return new int[] {};
-		if (limit < 2)
-			return new int[] {v};
-		
-		int i = 0;
-		int[] divisors = new int[30];
-		while ((v & 1) == 0) {
-			divisors[i++] = 2;
-			v >>>= 1;
-		}
-		if (limit > 2) {
-			while (v % 3 == 0) {
-				v /= 3;
-				divisors[i++] = 3;
-			}
-			for (int p = 5, d = 2; p <= limit & p * p <= v; p += d, d = 6-d) {
-				while (v % p == 0) {
-					v /= p;
-					divisors[i++] = p;
-				}
-			}
-		}
-		if (v > 1)
-			divisors[i++] = v;
-		return Arrays.copyOf(divisors, i);
-	}
-
+	// - helper method
 	private static int factorialDivCounts(int n, int divisor) {
 		if (divisor <= 1)
 			throw new RuntimeException();
@@ -115,17 +59,35 @@ public class IntegersModuloField {
 		return counts;
 	}
 
-	private static BitSet generateCompositeNumbersSet(int limit) {
-		BitSet isComposite = new BitSet(limit+1);
-		isComposite.set(0); isComposite.set(1);
-		for (int j = 4; 0 <= j & j <= limit; j+=2) {
-			isComposite.set(j);
-		}
-		for (int i = 3, limitSqrt = 1 + (int)Math.sqrt(limit); 0 < i & i <= limitSqrt; i = isComposite.nextClearBit(i+1)) {
-			for (int j = i*i; 0 <= j & j <= limit; j+=i+i) {
-				isComposite.set(j);
+	public int nCk(final int n, final int k) {
+		if (n-k < k)
+			return nCk(n, n-k);
+
+		BitSet isComposite = new BitSet(k+1);
+		int[] remainders = new int[k+1];
+		for (int i = 1; i <= k; i++)
+			remainders[i] = n-k+i;
+
+		int result = 1;			
+		for (int i = 2; i <= k; i++) {
+			// - prime
+			if (!isComposite.get(i)) {
+				// - mark as not primes
+				for (int j = i*i; j <= k; j+=i) {
+					isComposite.set(j);
+				}
+				result = mul(result, pow(i, factorialDivCounts(n, i) - factorialDivCounts(n - k, i) - factorialDivCounts(k, i)));
+				// - update remainders
+				for (int j = i - (n-k) % i; j <= k; j += i)
+					while (remainders[j] % i == 0)
+						remainders[j] /= i;
 			}
 		}
-		return isComposite;
+		
+		for (int i = 1; i <= k; i++) {
+			if (remainders[i] > 0)
+				result = mul(result, remainders[i]);
+		}
+		return result;
 	}
 }
